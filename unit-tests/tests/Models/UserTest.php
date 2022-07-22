e3fb8483d39a512dbc9e78785bebab2c6a175791
Lucas <lucasr4394@gmail.com>
2022-07-22 17:55:20 -0300

    incluído mensagens de erro personalizadas

--- unit-tests/tests/Models/UserTest.php
+++ unit-tests/tests/Models/UserTest.php
@@ -12,6 +12,10 @@ class UserTest extends TestCase
 	use CreatesApplication;
 
 	/**
+	 * @expectedException PHPUnit\Framework\Error
+	 */
+
+	/**
 	 * @var \Faker\Generator
 	 */
 	protected $faker;
@@ -37,22 +41,22 @@ class UserTest extends TestCase
 		], $this->user->getUser());
 	}
 
-	// testar se o nome do usuario é válido
+	// testar se o nome do usuário é válido
 	public function testNomeEhValido(): void
 	{
-		$this->assertTrue($this->user->nomeEhValido('Lucas Rodrigues'));
+		$this->assertTrue($this->user->nomeEhValido('Lucas'), 'Nome do usuário é inválido!');
 	}
 
-	// testar se a idade do usuario é válida
+	// testar se a idade do usuário é válida
 	public function testIdadeEhValida(): void
 	{
-		$this->assertTrue($this->user->idadeEhValida(28));
+		$this->assertTrue($this->user->idadeEhValida(28), 'Idade inválida!');
 	}
 
-	// testar se o nome e a idade do usuario são válidos
+	// testar se o nome e a idade do usuário são válidos
 	public function testNomeEIdadeSaoValidos(): void
 	{
-		$this->assertTrue($this->user->nomeEIdadeSaoValidos('Lucas Rodrigues', 28));
+		$this->assertTrue($this->user->nomeEIdadeSaoValidos('Lucas Rodrigues', 28), 'Nome e idade inválidos!');
 	}
 
 	public function tearDown(): void
